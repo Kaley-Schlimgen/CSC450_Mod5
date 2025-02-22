0f0f4ff6e8fe8bdc2b529dae8702f1691a0f4ad3
Kaley-Schlimgen <kaleynicole89@gmail.com>
2025-02-22 14:13:54 -0700

    while loop added

--- CSC450_Mod5/main.cpp
+++ CSC450_Mod5/main.cpp
@@ -11,18 +11,21 @@
 using namespace std;
 
 int main () {
+    char userInput;
     ifstream providedFile;
     string filePath = "/Users/kaleyschlimgen/Desktop/CSC450_CT5_mod5.txt";
 
-    //open file
+    //open file and read contents of file
     providedFile.open(filePath);
-    
     if (providedFile.is_open()) {
+        cout << "File Opened Successfully\n";
+        cout << "File Contents:\n";
         string line;
-        
+
         //read the file contents
         while (getline(providedFile, line)) {
             cout << line << endl;
+            
         }
 
         providedFile.close();
@@ -32,6 +35,26 @@ int main () {
         return 1;
     }
     
-     
-        return 0;
+    cout << "Enter 'a' to append the file (enter any other character to quit)\n";
+    cin >> userInput;
+    
+    while (userInput == 'a') {
+        //append the file
+        ofstream appendFile(filePath, ios::app);
+        if (appendFile.is_open()) {
+            appendFile << "ADDED LINE\n";
+            appendFile.close();
+            cout << "APPEND SUCCESSFUL\n";
+        }
+        else {
+            cout <<"APPEND NOT SUCCESSFUL\n";
+            return 1;
+        }
+   
+        cout << "Enter 'a' to append the file (enter any other character to quit)\n";
+        cin >> userInput;
+    }
+    
+    cout <<"Exiting Program\n";
+    return 0;
 }
