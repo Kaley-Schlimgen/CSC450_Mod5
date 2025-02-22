cbb33bf2c27cf957ff8d56f812dde2ae16a61c53
Kaley-Schlimgen <kaleynicole89@gmail.com>
2025-02-22 11:22:50 -0700

    Initial Commit

---
+++ CSC450_Mod5/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CSC450_Mod5
+//
+//  Created by Kaley Schlimgen on 2/22/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
