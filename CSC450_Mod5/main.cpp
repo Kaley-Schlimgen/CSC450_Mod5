a69634aeaaf6cf74f5ed4a377de0828c1205a032
Kaley-Schlimgen <kaleynicole89@gmail.com>
2025-02-22 12:02:46 -0700

    first commit

--- CSC450_Mod5/main.cpp
+++ CSC450_Mod5/main.cpp
@@ -6,9 +6,32 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main () {
+    ifstream providedFile;
+    string filePath = "/Users/kaleyschlimgen/Desktop/CSC450_CT5_mod5.txt";
+
+    //open file
+    providedFile.open(filePath);
+    
+    if (providedFile.is_open()) {
+        string line;
+        
+        //read the file contents
+        while (getline(providedFile, line)) {
+            cout << line << endl;
+        }
+
+        providedFile.close();
+    }
+    else {
+        cout << "Unable to open file";
+        return 1;
+    }
+    
+     
+        return 0;
 }
