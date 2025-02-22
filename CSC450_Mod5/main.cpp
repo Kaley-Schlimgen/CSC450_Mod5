b7d8b70ac0263e1477e493cdd166f8ad53be0461
Kaley-Schlimgen <kaleynicole89@gmail.com>
2025-02-22 15:02:05 -0700

    remove loop and remove print contents

--- CSC450_Mod5/main.cpp
+++ CSC450_Mod5/main.cpp
@@ -10,50 +10,53 @@
 #include <string>
 using namespace std;
 
+
 int main () {
-    char userInput;
+
+    string userInput;
     ifstream providedFile;
     string filePath = "/Users/kaleyschlimgen/Desktop/CSC450_CT5_mod5.txt";
-
+/*
     //open file and read contents of file
     providedFile.open(filePath);
     if (providedFile.is_open()) {
         cout << "File Opened Successfully\n";
         cout << "File Contents:\n";
+        cout << "\n";
         string line;
 
         //read the file contents
         while (getline(providedFile, line)) {
             cout << line << endl;
-            
+            cout << endl;
         }
-
         providedFile.close();
     }
     else {
         cout << "Unable to open file";
         return 1;
     }
-    
-    cout << "Enter 'a' to append the file (enter any other character to quit)\n";
-    cin >> userInput;
-    
-    while (userInput == 'a') {
+    */
         //append the file
         ofstream appendFile(filePath, ios::app);
         if (appendFile.is_open()) {
-            appendFile << "ADDED LINE\n";
+            
+            cout <<"Enter input to add to file:\n";
+            getline(cin, userInput);
+            
+            appendFile << "ADDED LINE:\n";
+            appendFile << userInput << endl;
+                
             appendFile.close();
+            
+            cout << endl;
             cout << "APPEND SUCCESSFUL\n";
+            cout << endl;
         }
         else {
             cout <<"APPEND NOT SUCCESSFUL\n";
             return 1;
         }
-   
-        cout << "Enter 'a' to append the file (enter any other character to quit)\n";
-        cin >> userInput;
-    }
     
     cout <<"Exiting Program\n";
     return 0;
